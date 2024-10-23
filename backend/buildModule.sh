2a259374a4b7322d5b25432edddc6ababbcc4de8
kimdohee58 <kimdohee58@gmail.com>
2024-10-23 20:47:55 +0900

    merge

--- backend/buildModule.sh
+++ backend/buildModule.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# IntelliJ IDEA JDK 경로, 실제 경로로 변경해주세요
-export JAVA_HOME="C:\\java"
-
 chmod +x ./gradlew
 
 # 모듈 리스트
@@ -27,5 +24,5 @@ echo "Cleaning..."
 for module in "${all_modules[@]}"
 do
     echo "Building for $module"
-    ./gradlew :$module:build
+    ./gradlew :$module:bootJar
 done
